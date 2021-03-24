eb95f9e6cd8ae8df83b14acc646b3a8b34e5cc64
Sean Kuehl <teddscottfelt@gmail.com>
2021-03-24 12:25:37 -0400

    added list categories in dir, list notes in dir and get current dir functions

---
+++ main/ListAllInDirectory.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+string get_current_dir();
+
+
+
+
+vector<string> GetCategoriesInDirectory(string path);
+
+vector<string> GetNotesInDirectory(string path);
+
+
